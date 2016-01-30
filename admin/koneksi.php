4d4e173f259fde69ee6dfa14f0996acde35ddc66
riansaepuloh <riansaepuloh92@gmail.com>
2016-01-30 17:32:12 +0700

    ubah isi koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user = "root";
 	$pass = "";
-	$db	  = "restoranbroto";
+	$db	  = "restoran_broto";
 
 	$koneksi=mysql_connect($host,$user,$pass) or die ("Tidak terkoneksi");
 	$pilih_db=mysql_select_db($db) or die ("Database tidak tersedia");
